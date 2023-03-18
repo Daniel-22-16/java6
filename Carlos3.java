d4f1518328fc1852ef36224e76dbe2b496a412f9
Daniel-22-16 <1475937senati.pe>
2023-03-18 08:26:33 -0500

    java6

--- Carlos3.java
+++ Carlos3.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 public class Carlos3 {
 
     public static void main(String[] args) {
-       Scanner sc = new Scanner(System.in);
-        System.out.println("ingrese su nombre");         
-        String nombre =sc.nextLine();
-        System.out.println("hola " + nombre);
-        
-        int suma = 0;
-        for (int i = 0; i<=5; i++){
-            System.out.println(suma + "+" +i );
-            suma = suma +i;
-            System.out.println("la suma de los 5 ptimeros numeros es: " + suma);
-        }
+       int numero =0;
+       Scanner sm = new Scanner(System.in);
+       System.out.println("porfavor ingrese un numero: ");
+       numero = sm.nextInt();
+       int suma = 0;
+       for(int i = 0;i<=numero;i++)
+       suma = suma +i;
+       {
+       System.out.println("la suma de los numeros es: " + suma);
+
     }
+}
 }
